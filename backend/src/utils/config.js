971e1d34206c80884c99f085a1df03adb6cbef79
Harshal Pawshekar <harshalpawshekar999@gmail.com>
2022-08-05 21:18:07 +0530

    Add more keys

--- backend/src/utils/config.js
+++ backend/src/utils/config.js
@@ -5,3 +5,11 @@ dotenv.config()
 export const PORT = process.env.PORT || 3001
 
 export const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/weather-app'
+
+export const JWT_SECRET = process.env.JWT_SECRET || 'secret'
+
+export const OPENAPI_KEY = process.env.OPENAPI_KEY
+
+export const API_NINJA_KEY = process.env.API_NINJA_KEY
+
+export const TWITTER_BEARER_TOKEN = process.env.TWITTER_BEARER_TOKEN

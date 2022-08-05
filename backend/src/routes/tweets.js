609a005dd622983551de0bf1a0fb9c2cb98a5e46
Harshal Pawshekar <harshalpawshekar999@gmail.com>
2022-08-05 21:22:00 +0530

    Add POST to get tweets

---
+++ backend/src/routes/tweets.js
@@ -0,0 +1,26 @@
+import {Router} from "express";
+import {body, validationResult} from "express-validator";
+import {getCityWeather, getTweets} from "../utils/utils.js";
+
+const tweetsRouter = Router()
+
+tweetsRouter.post('/',
+	body('lat').escape().isNumeric(),
+	body('lon').escape().isNumeric(),
+	async (request, response, next) => {
+		const errors = validationResult(request)
+		if (!errors.isEmpty()) {
+			return response.status(400).json({error: errors.array()})
+		}
+
+		const {lat, lon} = request.body
+		const cityWeather = await getCityWeather(lat, lon, 1)
+		const cityName = cityWeather.city.name
+
+		const tweets = await getTweets(cityName)
+
+		response.json(tweets)
+	}
+)
+
+export default tweetsRouter;

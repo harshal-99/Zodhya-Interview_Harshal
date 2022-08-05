459b73fb6333e78a2dd001ebbf5627fe0a28c7c9
Harshal Pawshekar <harshalpawshekar999@gmail.com>
2022-08-05 21:20:20 +0530

    Handle error

--- backend/src/utils/middleware.js
+++ backend/src/utils/middleware.js
@@ -1,3 +1,6 @@
+import jwt from "jsonwebtoken";
+import {JWT_SECRET} from "./config.js";
+
 export const tokenExtractor = (request, response, next) => {
 	const authorization = request.get('authorization')
 	if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
@@ -8,10 +11,31 @@ export const tokenExtractor = (request, response, next) => {
 
 
 export const errorHandler = (error, request, response, next) => {
+	console.log(error.name)
 	console.log(error.message)
+	switch (error.name) {
+		case 'ValidationError':
+			return response.status(400).json({error: error.message})
+		case 'JsonWebTokenError':
+			return response.status(401).json({error: 'invalid token'})
+		case 'TokenExpiredError':
+			return response.status(401).json({error: 'token expired'})
+	}
 	next(error)
 }
 
+export const tokenValidator = (request, response, next) => {
+	const token = request.token
+	if (!token) {
+		return response.status(401).json({error: "token missing"})
+	}
+
+	const decodedToken = jwt.verify(token, JWT_SECRET)
+	if (!decodedToken.id) {
+		return response.status(401).json({error: "invalid token Login again"})
+	}
+	next()
+}
 
 export const unknownEndpoint = (request, response) => {
 	response.status(404).send({error: "unknown endpoint"})

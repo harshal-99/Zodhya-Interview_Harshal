91b26554170bdf460468e3c84a0f37005c093a9f
Harshal Pawshekar <harshalpawshekar999@gmail.com>
2022-08-05 21:18:42 +0530

    Add url for rapid api, open api and Twitter api

---
+++ backend/src/utils/constants.js
@@ -0,0 +1,3 @@
+export const rapid_api_url = 'https://api.api-ninjas.com/v1/city'
+export const open_api_url = 'https://api.openweathermap.org/data/2.5/forecast'
+export const twitter_api_url = 'https://api.twitter.com/2/tweets/search/recent'
